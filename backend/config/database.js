5df3f39b62d0bad1aeedcc978cab047a30b5f311
mechergui  mohamed ali <mecherguimouhamed94@gmail.com>
2024-11-06 14:05:46 +0100

    config vps

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,7 @@ const app = express();
 const mongoose = require('mongoose');
 
 
-var MONGODB_URI = process.env.MONGODB_URI;
+var MONGODB_URI = process.env.MONGODB_URI_local;
 var PORT = process.env.PORT
 mongoose.connect(MONGODB_URI).then(()=>{
     console.log('connected to MongoDB local ');
